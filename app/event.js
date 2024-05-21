e087c3818c9f8c8852ba306b1a47f3426b41b455
RajaYogan05 <136057809+RajaYogan05@users.noreply.github.com>
2024-05-21 22:18:31 +0530

    Initial commit

---
+++ app/event.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+const Event = () => {
+  return (
+    <SafeAreaView>
+      <Text>Event</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Event;
+
+const styles = StyleSheet.create({});
